f972a157f83215467ee8a7dacbdb908d13282d36
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-11-23 12:52:26 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+let userName = prompt("Enter your name");
+// Test // Question-1
+let linkingCss = prompt(
+  "How many ways are there to link css? \n a) 5 \n b) 8 \n c) 3 "
+);
+console.log(
+  "Username:" +
+    userName +
+    linkingCss +
+    "\n" +
+    "Question: How many ways are there to link css?" +
+    "\n" +
+    "User answer:" +
+    linkingCss +
+    " " +
+    "Correct answer: c"
+);
+
+// Question-2
+let backImg = prompt(
+  "Is there a property called Background image in css \n a) No \n b) yes there is"
+);
+console.log(
+  "Username:" +
+    userName +
+    "\n" +
+    "Question: Is there a property called Background image in css?" +
+    "\n" +
+    "User answer:" +
+    backImg +
+    " " +
+    "Correct answer: b = Yes There is, For instance: Background-image: url()"
+);
+
+// Question-3
+let programmingLgOr = prompt(
+  "Is JavaScript programming language or ... ? \n a) Yes it is language \n b) No It is not \n c) Js is for styling texts"
+);
+console.log(
+  "Username:" +
+    userName +
+    "\n" +
+    "Question: Is JavaScript programming language or ...?" +
+    "\n" +
+    "User answer:" +
+    programmingLgOr +
+    " " +
+    "Correct answer: a = JavaScript is the most popular programming language (Do not forget :)"
+);
+
+// Question-4
+let isObject = prompt(
+  "Is there a data type called (Object) in JavaScript? \n a) Yes \n b) Html has an object data type \n c) There are no data types in JavaScript"
+);
+console.log(
+  "Username:" +
+    userName +
+    "\n" +
+    "Question: Is there a data type called (Object) in JavaScript?" +
+    "\n" +
+    "User answer:" +
+    isObject +
+    " " +
+    "Correct answer: a = JavaScript has a data type called Object, and you can see all the data types in the last question :) "
+);
+
+// Question-5
+let dataTypes = prompt(
+  "How many data types are there in JavaScript? \n a) 3 \n b) 8 \n c) 6"
+);
+console.log(
+  "Username:" +
+    userName +
+    "\n" +
+    "Question: How many data types are there in JavaScript?" +
+    "\n" +
+    "User answer:" +
+    dataTypes +
+    " " +
+    "Correct answer: b = There are 8 data types in JavaScript \n 1.Boolean \n 2.String \n 3.Number \n 4.Big int \n 5.Symbol \n 6.Object \n 7.Undefined \n 5.Null"
+);
